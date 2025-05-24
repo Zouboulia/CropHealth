330dca609b438ad517c3d518617f739927ac5c1d
Zouboulia <78475889+Zouboulia@users.noreply.github.com>
2025-05-24 18:01:44 +0100

    Added AllCrops.razor page to show all crops + soil data, Created CropsController API endpoint to fetch crop and field info, Enhanced recommendations to consider field location and match with city UI input, Fixed issues with model references, Cleaned up dev test data + improved database structure (Field to SoilData link)

--- Smartcrop.Shared/Models/Field.cs
+++ Smartcrop.Shared/Models/Field.cs
@@ -9,5 +9,7 @@ public class Field
     public int FarmerId { get; set; } // This is the foreign key to the Farmer table
     public Farmer Farmer { get; set; } // This is the navigation property to the Farmer table
 
+    public SoilData SoilData { get; set; } // This will be the soil data associated with the field
+    
     public ICollection<Crop> Crops { get; set; } // This will be a collection of crops planted in the field
 }
