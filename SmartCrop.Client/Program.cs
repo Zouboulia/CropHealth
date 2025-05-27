9633d5661db5102c2bb9d0200aed953ca59d71e6
Zouboulia <78475889+Zouboulia@users.noreply.github.com>
2025-05-27 10:17:56 +0100

    - Refactored recommendations method in MonitoringService.cs into separate methods for weather, pH and SOM recommendations, ad per linting suggestion - fixed linting issue with URi & other repetitions

--- SmartCrop.Client/Program.cs
+++ SmartCrop.Client/Program.cs
@@ -3,9 +3,6 @@ using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5204/") });
-
-
 // Load configuration from appsettings.json instead of hardcoding the base URL as per linting recommendations
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
@@ -14,6 +11,5 @@ var baseUrl = builder.Configuration.GetValue<string>("ApiSettings:BaseUrl");
 
 // Configure HttpClient with the base URL from configuration
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
-
 
 await builder.Build().RunAsync();
